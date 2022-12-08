4ab5ba6d63fcd3a5a089f682141883e2ee8497f7
mzuhriwijianto <zuhrywijianto@gmail.com>
2022-12-08 21:13:27 +0700

    aisah-vue

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Modul Tanah</template>
+
+    Tanah Pemerintah Kabupaten Bojonegoro
+    Meliputi Aset Tetap Tanah Rincian Tanah, jumlah tanah, bukti kepemilikan dan Lokasi Tanah.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Kendaraan Dinas</template>
+    Kendaraan Dinas Pemerintah Kabupaten Bojonegoro
+    Meliputi Aset Tetap Kendaraan Rincian Kendaraan, jumlah kendaraan, bukti kepemilikan dan Pemegang Kendaraan.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Gedung dan Bangunan</template>
+
+    Gedung dan Bangunan Pemerintah Kabupaten Bojonegoro
+    Meliputi Aset Tetap Kendaraan Rincian Kendaraan, jumlah kendaraan, bukti kepemilikan dan Pemegang Kendaraan.
+  </WelcomeItem>
+</template>
